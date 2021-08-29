7c39c1c6684ad5089cd43d9027e3de7af1705339
vlearn-tech <vlearn.tech.official@gmail.com>
2021-08-29 10:00:46 +0530

    Data Types

---
+++ java-training-b20210826/src/com/vlearntech/java/step03datatypes/DataTypes.java
@@ -0,0 +1,25 @@
+package com.vlearntech.java.step03datatypes;
+
+public class DataTypes {
+	public static void main(String[] args) {
+
+		boolean isMarried = true;
+
+		char gradeInExam = 'A';
+
+		byte noOfStudents = 100;
+
+		short noOfInvitesForParty = 450;
+
+		int salary = 50_000;
+
+		long populationOfContinent = 4_000_000_000L; // If you enter a number beyond 2.14 b (Integer.MAX_VALUE), you
+														// need to add a l or L at the end of the numeric literal.
+
+		float avgAge = 23.6f; // Decimals are by default "double", and hence you need "f" to be added at the end to
+								// indicate that it is a float value
+
+		double avgSalary = 45342.56;
+
+	}
+}
---
+++ java-training-b20210826/src/com/vlearntech/java/step03datatypes/Variables.java
@@ -0,0 +1,15 @@
+package com.vlearntech.java.step03datatypes;
+
+public class Variables {
+	public static void main(String[] args) {
+
+		int noofcharactersinword = 5; // not good
+
+		int noOfCharactersInWord = 5; // clear and should be used
+
+		final int MAX_LENGTH_ALLOWED = 10; // final means this variable is a constant and cannot be changed
+
+		System.out.println(Double.SIZE);
+
+	}
+}
